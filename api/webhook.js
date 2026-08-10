ec306ee0c29819f1101be59aa25b90ff3d45a239
skyler-stack <skyler@bloxspring.com>
2026-08-10 16:00:19 +0100

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,4 +1,22 @@
-export default function handler(req, res) {
-  console.log('收到HubSpot消息:', JSON.stringify(req.body, null, 2));
-  res.status(200).json({ received: true });
+export default async function handler(req, res) {
+  const token = process.env.HUBSPOT_TOKEN;
+
+  const response = await fetch('https://api.hubapi.com/crm/v3/objects/deals/search', {
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer ${token}`,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      filterGroups: [{
+        filters: [{ propertyName: 'dealstage', operator: 'EQ', value: 'closedwon' }]
+      }],
+      sorts: [{ propertyName: 'hs_lastmodifieddate', direction: 'DESCENDING' }],
+      properties: ['dealname', 'amount', 'hs_lastmodifieddate'],
+      limit: 10
+    })
+  });
+
+  const data = await response.json();
+  res.status(200).json(data.results);
 }
