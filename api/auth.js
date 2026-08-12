40ed2df825f0517bf6c1234454bc503c99548a22
skyler-stack <skyler@bloxspring.com>
2026-08-12 17:21:17 +0100

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
   const clientId = process.env.XERO_CLIENT_ID;
   const redirectUri = 'https://hubspot-xero.vercel.app/api/callback';
-  const scope = 'openid profile email accounting.transactions accounting.contacts offline_access';
+  const scope = 'offline_access accounting.contacts accounting.invoices';
 
   const authUrl = `https://login.xero.com/identity/connect/authorize?response_type=code&client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${encodeURIComponent(scope)}`;
 
